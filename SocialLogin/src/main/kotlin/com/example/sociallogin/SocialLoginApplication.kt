96247b199d8c314f7558eed613a9119af518d8a9
programofktw <thkim000317@naver.com>
2024-10-29 10:02:50 +0900

    [Feat] : kotlin spring project init

---
+++ SocialLogin/src/main/kotlin/com/example/sociallogin/SocialLoginApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sociallogin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialLoginApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialLoginApplication>(*args)
+}
