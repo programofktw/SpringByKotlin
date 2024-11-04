61fb46dfde92420ce90652193f11d3bfaef62a2e
programofktw <thkim000317@naver.com>
2024-11-04 13:02:12 +0900

    [delete] : re project start

--- Spring/src/main/kotlin/com/example/spring/social/oauth2/common/entity/dto/userInfo/OAuth2UserInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spring.social.oauth2.common.entity.dto.userInfo
-
-interface OAuth2UserInfo {
-    fun getProviderId() : String
-    fun getProvider() : String
-    fun getName() : String
-}
--- Spring/src/main/kotlin/com/example/spring/social/oauth2/common/jwt/error/TokenErrorResult.kt
+++
@@ -1,4 +0,0 @@
-package com.example.spring.social.oauth2.common.jwt.error
-
-class TokenErrorResult {
-}
