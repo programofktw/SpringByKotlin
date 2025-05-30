3ba19a4f5e6cdc98448d69dcce4ddd44dd4cd119
programofktw <thkim000317@naver.com>
2025-05-30 18:26:40 +0900

    [SETTING] project init

---
+++ SideProject/src/main/kotlin/com/example/sideproject/SideProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sideproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SideProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SideProjectApplication>(*args)
+}
