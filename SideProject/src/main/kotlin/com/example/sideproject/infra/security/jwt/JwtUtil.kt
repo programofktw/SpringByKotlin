97c83643cb1e0a9f65b3169e21e44cbc889ecb83
programofktw <thkim000317@naver.com>
2025-06-11 17:35:18 +0900

    [FEAT] Component 설정 및 logger 추가

--- SideProject/src/main/kotlin/com/example/sideproject/infra/security/jwt/JwtUtil.kt
+++ SideProject/src/main/kotlin/com/example/sideproject/infra/security/jwt/JwtUtil.kt
@@ -1,4 +1,12 @@
 package com.example.sideproject.infra.security.jwt
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
 class JwtUtil {
+    var log : Logger = LoggerFactory.getLogger(this::class.java)
+
+
 }
